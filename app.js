3a967d64b11485d8cd876f5c42c8096481e9cd35
vtbano <vtbano@gmail.com>
2022-03-13 17:05:25 -0400

    add try and catch to playGame & nextRound

--- app.js
+++ app.js
@@ -1,81 +1,89 @@
 const playGame = async () => {
-  const response = await fetch(
-    `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=hot&page=1&page_size=50&country=US&f_has_lyrics=1&apikey=${apiKey}`
-  );
-  const data = await response.json();
-  const body = data.message.body;
-  const getRandomNumber = (max) => Math.floor(Math.random() * max);
-  const songCount = body.track_list.length;
-  const getRandomName = getRandomNumber(songCount);
-  const songName = body.track_list[getRandomName].track.track_name; // const song name
-  const artistName = body.track_list[getRandomName].track.artist_name; // const artist name
-  const artistName2 =
-    body.track_list[getRandomNumber(songCount)].track.artist_name;
-  const artistName3 =
-    body.track_list[getRandomNumber(songCount)].track.artist_name;
-
-  const artistArray = [artistName, artistName2, artistName3];
-
-  const mixedArtistOrder = shuffle(artistArray);
-
-  const landingPageImage = document.querySelector("#landing-page-img");
-  landingPageImage.remove();
-
-  const gameDescription = document.querySelector(".gameDescription");
-  gameDescription.remove();
-
-  const playButton = document.querySelector(".playButton");
-  playButton.remove();
-
-  const bottomAndRight = document.querySelector("#bottom-and-right");
-
-  const firstButton = document.createElement("button");
-  firstButton.classList.add("firstButton");
-  firstButton.setAttribute("id", "optionButton");
-  firstButton.textContent = mixedArtistOrder[0];
-  bottomAndRight.appendChild(firstButton);
-
-  const secondButton = document.createElement("button");
-  secondButton.classList.add("secondButton");
-  secondButton.setAttribute("id", "optionButton");
-  secondButton.textContent = mixedArtistOrder[1];
-  bottomAndRight.appendChild(secondButton);
-
-  const thirdButton = document.createElement("button");
-  thirdButton.classList.add("thirdButton");
-  thirdButton.setAttribute("id", "optionButton");
-  thirdButton.textContent = mixedArtistOrder[2];
-  bottomAndRight.appendChild(thirdButton);
-
-  const getLyrics = async () => {
+  try {
     const response = await fetch(
-      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${songName}&q_artist=${artistName}&apikey=${apiKey}`
+      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=hot&page=1&page_size=50&country=US&f_has_lyrics=1&apikey=${apiKey}`
     );
     const data = await response.json();
     const body = data.message.body;
-    const requiredLyrics = body.lyrics.lyrics_body;
-    const topAndLeft = document.querySelector("#top-and-left");
-    const showLyrics = document.createElement("div");
-    showLyrics.classList.add("lyrics-container");
-    showLyrics.textContent = requiredLyrics;
-    topAndLeft.appendChild(showLyrics);
-  };
+    const getRandomNumber = (max) => Math.floor(Math.random() * max);
+    const songCount = body.track_list.length;
+    const getRandomName = getRandomNumber(songCount);
+    const songName = body.track_list[getRandomName].track.track_name; // const song name
+    const artistName = body.track_list[getRandomName].track.artist_name; // const artist name
+    const artistName2 =
+      body.track_list[getRandomNumber(songCount)].track.artist_name;
+    const artistName3 =
+      body.track_list[getRandomNumber(songCount)].track.artist_name;
 
-  getLyrics();
-  const optionButtons = document.querySelectorAll("button");
-  optionButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const optionSelectedContent = button.textContent;
-      optionSelectedView(
-        optionSelectedContent,
-        artistName,
-        bottomAndRight,
-        firstButton,
-        secondButton,
-        thirdButton
-      );
+    const artistArray = [artistName, artistName2, artistName3];
+
+    const mixedArtistOrder = shuffle(artistArray);
+
+    const landingPageImage = document.querySelector("#landing-page-img");
+    landingPageImage.remove();
+
+    const gameDescription = document.querySelector(".gameDescription");
+    gameDescription.remove();
+
+    const playButton = document.querySelector(".playButton");
+    playButton.remove();
+
+    const bottomAndRight = document.querySelector("#bottom-and-right");
+
+    const firstButton = document.createElement("button");
+    firstButton.classList.add("firstButton");
+    firstButton.setAttribute("id", "optionButton");
+    firstButton.textContent = mixedArtistOrder[0];
+    bottomAndRight.appendChild(firstButton);
+
+    const secondButton = document.createElement("button");
+    secondButton.classList.add("secondButton");
+    secondButton.setAttribute("id", "optionButton");
+    secondButton.textContent = mixedArtistOrder[1];
+    bottomAndRight.appendChild(secondButton);
+
+    const thirdButton = document.createElement("button");
+    thirdButton.classList.add("thirdButton");
+    thirdButton.setAttribute("id", "optionButton");
+    thirdButton.textContent = mixedArtistOrder[2];
+    bottomAndRight.appendChild(thirdButton);
+
+    const getLyrics = async () => {
+      try {
+        const response = await fetch(
+          `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${songName}&q_artist=${artistName}&apikey=${apiKey}`
+        );
+        const data = await response.json();
+        const body = data.message.body;
+        const requiredLyrics = body.lyrics.lyrics_body;
+        const topAndLeft = document.querySelector("#top-and-left");
+        const showLyrics = document.createElement("div");
+        showLyrics.classList.add("lyrics-container");
+        showLyrics.textContent = requiredLyrics;
+        topAndLeft.appendChild(showLyrics);
+      } catch (err) {
+        getLyrics();
+      }
+    };
+
+    getLyrics();
+    const optionButtons = document.querySelectorAll("button");
+    optionButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const optionSelectedContent = button.textContent;
+        optionSelectedView(
+          optionSelectedContent,
+          artistName,
+          bottomAndRight,
+          firstButton,
+          secondButton,
+          thirdButton
+        );
+      });
     });
-  });
+  } catch (err) {
+    playGame();
+  }
 };
 
 const playButton = document.querySelector(".playButton");
@@ -101,75 +109,83 @@ const shuffle = (array) => {
 };
 
 const nextRound = async () => {
-  const response = await fetch(
-    `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=hot&page=1&page_size=50&country=US&f_has_lyrics=1&apikey=${apiKey}`
-  );
-  const data = await response.json();
-  const body = data.message.body;
-  const getRandomNumber = (max) => Math.floor(Math.random() * max);
-  const songCount = body.track_list.length;
-  const getRandomName = getRandomNumber(songCount);
-  const songName = body.track_list[getRandomName].track.track_name; // const song name
-  const artistName = body.track_list[getRandomName].track.artist_name; // const artist name
-  const artistName2 =
-    body.track_list[getRandomNumber(songCount)].track.artist_name;
-  const artistName3 =
-    body.track_list[getRandomNumber(songCount)].track.artist_name;
-
-  const artistArray = [artistName, artistName2, artistName3];
-
-  const mixedArtistOrder = shuffle(artistArray);
-
-  const bottomAndRight = document.querySelector("#bottom-and-right");
-
-  const firstButton = document.createElement("button");
-  firstButton.classList.add("firstButton");
-  firstButton.setAttribute("id", "optionButton");
-  firstButton.textContent = mixedArtistOrder[0];
-  bottomAndRight.appendChild(firstButton);
-
-  const secondButton = document.createElement("button");
-  secondButton.classList.add("secondButton");
-  secondButton.setAttribute("id", "optionButton");
-  secondButton.textContent = mixedArtistOrder[1];
-  bottomAndRight.appendChild(secondButton);
-
-  const thirdButton = document.createElement("button");
-  thirdButton.classList.add("thirdButton");
-  thirdButton.setAttribute("id", "optionButton");
-  thirdButton.textContent = mixedArtistOrder[2];
-  bottomAndRight.appendChild(thirdButton);
-
-  const getLyrics = async () => {
+  try {
     const response = await fetch(
-      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${songName}&q_artist=${artistName}&apikey=${apiKey}`
+      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=hot&page=1&page_size=50&country=US&f_has_lyrics=1&apikey=${apiKey}`
     );
     const data = await response.json();
     const body = data.message.body;
-    const requiredLyrics = body.lyrics.lyrics_body;
-    const topAndLeft = document.querySelector("#top-and-left");
-    const showLyrics = document.createElement("div");
-    showLyrics.classList.add("lyrics-container");
-    showLyrics.textContent = requiredLyrics;
-    topAndLeft.appendChild(showLyrics);
-  };
+    const getRandomNumber = (max) => Math.floor(Math.random() * max);
+    const songCount = body.track_list.length;
+    const getRandomName = getRandomNumber(songCount);
+    const songName = body.track_list[getRandomName].track.track_name; // const song name
+    const artistName = body.track_list[getRandomName].track.artist_name; // const artist name
+    const artistName2 =
+      body.track_list[getRandomNumber(songCount)].track.artist_name;
+    const artistName3 =
+      body.track_list[getRandomNumber(songCount)].track.artist_name;
 
-  getLyrics();
+    const artistArray = [artistName, artistName2, artistName3];
 
-  const optionButtons = document.querySelectorAll("button");
-  optionButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const optionSelectedContent = button.textContent;
-      optionSelectedView(
-        optionSelectedContent,
-        artistName,
-        bottomAndRight,
-        firstButton,
-        secondButton,
-        thirdButton
-      );
+    const mixedArtistOrder = shuffle(artistArray);
+
+    const bottomAndRight = document.querySelector("#bottom-and-right");
+
+    const firstButton = document.createElement("button");
+    firstButton.classList.add("firstButton");
+    firstButton.setAttribute("id", "optionButton");
+    firstButton.textContent = mixedArtistOrder[0];
+    bottomAndRight.appendChild(firstButton);
+
+    const secondButton = document.createElement("button");
+    secondButton.classList.add("secondButton");
+    secondButton.setAttribute("id", "optionButton");
+    secondButton.textContent = mixedArtistOrder[1];
+    bottomAndRight.appendChild(secondButton);
+
+    const thirdButton = document.createElement("button");
+    thirdButton.classList.add("thirdButton");
+    thirdButton.setAttribute("id", "optionButton");
+    thirdButton.textContent = mixedArtistOrder[2];
+    bottomAndRight.appendChild(thirdButton);
+
+    const getLyrics = async () => {
+      try {
+        const response = await fetch(
+          `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${songName}&q_artist=${artistName}&apikey=${apiKey}`
+        );
+        const data = await response.json();
+        const body = data.message.body;
+        const requiredLyrics = body.lyrics.lyrics_body;
+        const topAndLeft = document.querySelector("#top-and-left");
+        const showLyrics = document.createElement("div");
+        showLyrics.classList.add("lyrics-container");
+        showLyrics.textContent = requiredLyrics;
+        topAndLeft.appendChild(showLyrics);
+      } catch (err) {
+        getLyrics();
+      }
+    };
+
+    getLyrics();
+
+    const optionButtons = document.querySelectorAll("button");
+    optionButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const optionSelectedContent = button.textContent;
+        optionSelectedView(
+          optionSelectedContent,
+          artistName,
+          bottomAndRight,
+          firstButton,
+          secondButton,
+          thirdButton
+        );
+      });
     });
-  });
+  } catch (err) {
+    nextRound();
+  }
 };
 
 const optionSelectedView = (
