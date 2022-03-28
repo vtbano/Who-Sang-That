367ab010d28530722e7d64f2f67115cf4d4e184b
vtbano <vtbano@gmail.com>
2022-03-28 11:34:43 -0400

    add util.js for tests

--- app.js
+++ app.js
@@ -76,39 +76,6 @@ const nextRound = async (score) => {
   }
 };
 
-const getRandomNumber = (max) => Math.floor(Math.random() * max);
-
-const generateArtistName2 = (body, artistName) => {
-  const secondArtist =
-    body.track_list[getRandomNumber(body.track_list.length)].track.artist_name;
-  if (secondArtist === artistName) {
-    console.log("Duplication: Regenerate Artist 2");
-    return generateArtistName2(body, artistName);
-  } else if (secondArtist.length === 0) {
-    console.log("Blank: Regenerate Artist 2");
-    return generateArtistName2(body, artistName);
-  } else {
-    return secondArtist;
-  }
-};
-
-const generateArtistName3 = (body, artistName, artistName2) => {
-  const thirdArtist =
-    body.track_list[getRandomNumber(body.track_list.length)].track.artist_name;
-  if (thirdArtist === artistName) {
-    console.log("Duplication of main artist: Regenerate Artist 3");
-    return generateArtistName3(body, artistName, artistName2);
-  } else if (thirdArtist === artistName2) {
-    console.log("Duplication of second artist: Regenerate Artist 3");
-    return generateArtistName3(body, artistName, artistName2);
-  } else if (thirdArtist.length === 0) {
-    console.log("Blank: Regenerate Artist 3");
-    return generateArtistName3(body, artistName, artistName2);
-  } else {
-    return thirdArtist;
-  }
-};
-
 const playButton = document.querySelector(".play-button");
 playButton.addEventListener("click", playGame);
 
@@ -400,9 +367,3 @@ const displayNextRoundButton = () => {
     nextRound(initialscore);
   });
 };
-
-// module.exports = {
-//   getRandomNumber,
-//   generateArtistName2,
-//   generateArtistName3,
-// };
