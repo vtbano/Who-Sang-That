3a9d2499f030ec0d25cb4ed911facc4720b11d8a
vtbano <vtbano@gmail.com>
2022-03-29 21:36:31 -0400

    remove commented out lines in appTests.js

--- appTests.js
+++ appTests.js
@@ -4,39 +4,6 @@ const {
   generateArtistName3,
 } = require("./util.js");
 
-// const getRandomNumber = (max) => Math.floor(Math.random() * max);
-
-// const generateArtistName2 = (body, artistName) => {
-//   const secondArtist =
-//     body.track_list[getRandomNumber(body.track_list.length)].track.artist_name;
-//   if (secondArtist === artistName) {
-//     console.log("Duplication: Regenerate Artist 2");
-//     return generateArtistName2(body, artistName);
-//   } else if (secondArtist.length === 0) {
-//     console.log("Blank: Regenerate Artist 2");
-//     return generateArtistName2(body, artistName);
-//   } else {
-//     return secondArtist;
-//   }
-// };
-
-// const generateArtistName3 = (body, artistName, artistName2) => {
-//   const thirdArtist =
-//     body.track_list[getRandomNumber(body.track_list.length)].track.artist_name;
-//   if (thirdArtist === artistName) {
-//     console.log("Duplication of main artist: Regenerate Artist 3");
-//     return generateArtistName3(body, artistName, artistName2);
-//   } else if (thirdArtist === artistName2) {
-//     console.log("Duplication of second artist: Regenerate Artist 3");
-//     return generateArtistName3(body, artistName, artistName2);
-//   } else if (thirdArtist.length === 0) {
-//     console.log("Blank: Regenerate Artist 3");
-//     return generateArtistName3(body, artistName, artistName2);
-//   } else {
-//     return thirdArtist;
-//   }
-// };
-
 const trackListMockResponse = {
   track_list: [
     {
